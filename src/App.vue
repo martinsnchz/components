93b2cfb6a776156c875beda9938f77e44480dd2c
martinsnchz <martin.snchz@eduge.ch>
2018-05-23 14:31:32 +0200

    premier commitx

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <button v-on:click="component = 'page-wright'">Freres</button>
+      <button v-on:click="component = 'page-amelia'">Amelia</button>
+      <component v-bind:is="component"></component>
+      <img :src="img">
+    </div>
+</template>
+
+<script>
+// Imports
+import wright from './components/wright.vue';
+import amelia from './components/amelia.vue'
+
+
+export default {
+    components: {
+        'page-wright': wright,
+        'page-amelia': amelia
+    },
+    data () {
+        return {
+          component: 'page-wright',
+          img: require('./assets/image1.jpg'),
+        }
+    },
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/amelia.vue
@@ -0,0 +1,9 @@
+import image1 from './assets/image1.jpg'
+
+<template>
+  <div>
+    <h1>Amelia Earhart</h1>
+    <p>Amelia Earhart, née à Atchison dans le Kansas le 24 juillet 1897 et disparue dans la région de l'archipel des îles Phœnix (actuelles Kiribati) le 2 juillet 1937, est une aviatrice américaine. Elle est célèbre notamment pour avoir été, en juin 1928, la première femme à traverser l'océan Atlantique en avion puis, en 1932, la première femme à le traverser en solitaire.
+    </p>
+  </div>
+</template>
